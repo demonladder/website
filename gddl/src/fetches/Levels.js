76e0532b90c3353ef4782a95bcef9b60a834609c
DiversenSato <diversensato@gmail.com>
2023-03-01 14:05:38 +0100

    reduced fetch timeouts

--- gddl/src/fetches/Levels.js
+++ gddl/src/fetches/Levels.js
@@ -1,20 +1,28 @@
 import serverIP from "../serverIP";
 
 async function byName(name, chunk = 5) {
-    return fetch(`${serverIP}/getLevels?name=${name}&chunk=${chunk}`, {
-        credentials: 'include'
-    }).then(res => {
-        return res.json();
-    }).catch(e => { throw new Error('Couldn\'t connect to the server!'); });
+    return fetchTimeout(`${serverIP}/getLevels?name=${name}&chunk=${chunk}`)
+    .then(res => res.json());
 }
 
-async function byPack(name, id, chunk = 5) {
-    return fetch(`${serverIP}/getPack?packID=${id}`, {
-        credentials: 'include'
-    }).then(res => res.json()).then(levels => {
-        return levels.Levels.filter(l => l.Name.startsWith(name));
-    })
-    .catch(e => {});
+async function byPack(name, id) {
+    return fetchTimeout(`${serverIP}/getPack?packID=${id}`)
+    .then(res => res.json())
+    .then(levels => levels.Levels.filter(l => l.Name.startsWith(name)));
+}
+
+function fetchTimeout(path, options = {}) {
+    const { timeout = 5000 } = options;
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+
+    return fetch(path, {
+        ...options,
+        signal: controller.signal
+    }).then(res => {
+        clearTimeout(id);
+        return res;
+    });
 }
 
 const FetchLevels = {
