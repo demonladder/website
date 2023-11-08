ccf912ad540fe1f5e0f3fb51cf8c3019f53b0d52
DiversenSato <diversensato@gmail.com>
2023-11-08 17:13:28 +0100

    Notifs

--- gddl/src/components/ui/Notifications.tsx
+++ gddl/src/components/ui/Notifications.tsx
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { GetNotifications, NotificationResponse } from '../../api/notifications/requests/GetNotificationsRequest';
 import { useState } from 'react';
 import { MarkReadNotifications } from '../../api/notifications/requests/MarkReadNotifications';
 
 export default function Notifications() {
     const [show, setShow] = useState(false);
+    const queryClient = useQueryClient();
     const { data } = useQuery({
         queryKey: ['notifications'],
         queryFn: GetNotifications,
@@ -18,16 +19,18 @@ export default function Notifications() {
         }
 
         setShow(true);
-        MarkReadNotifications(data.slice(0, 5).filter((notif) => !notif.IsRead).map((notif) => notif.ID));
+        MarkReadNotifications(data.slice(0, 5).filter((notif) => !notif.IsRead).map((notif) => notif.ID))?.then(() => {
+            queryClient.invalidateQueries(['notifications']);
+        });
     }
 
     return (
         <div className='cursor-pointer relative'>
-            {(data?.length || 0) > 0
+            {(data?.filter((n) => !n.IsRead).length || 0) > 0
                 ? <i className='bx bxs-envelope text-2xl' onClick={openNotifications}></i>
                 : <i className='bx bx-envelope-open text-2xl' onClick={openNotifications}></i>
             }
-            <div className={'absolute w-72 left-1/2 -translate-x-1/2 bg-gray-500 text-white round:rounded-lg transition-opacity ' + (show ? 'opacity-100' : 'opacity-0')}>
+            <div className={'absolute w-72 z-10 left-1/2 -translate-x-1/2 bg-gray-500 text-white round:rounded-lg transition-opacity ' + (show ? 'opacity-100' : 'opacity-0')} id='notificationsBox'>
                 <div className='flex justify-between'>
                     <h4 className='font-bold p-2'>Notifications</h4>
                     <button className='px-2 text-2xl' onClick={() => setShow(false)}><i className='bx bx-x'></i></button>
