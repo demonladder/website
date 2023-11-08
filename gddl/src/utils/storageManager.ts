6f65499cca25191b3b9b1283bd3a23c2c950d04c
DiversenSato <diversensato@gmail.com>
2023-11-08 02:36:15 +0100

    Local settings

--- gddl/src/utils/storageManager.ts
+++ gddl/src/utils/storageManager.ts
@@ -7,6 +7,20 @@ export type User = {
     PermissionLevel: number,
 }
 
+interface Settings {
+    submission: {
+        defaultRefreshRate: number,
+    },
+}
+
+function generateDefaultSettings(): Settings {
+    return {
+        submission: {
+            defaultRefreshRate: 60,
+        },
+    };
+}
+
 export default {
     getUser(): User | null {
         return JSON.parse(''+localStorage.getItem('user'));
@@ -59,4 +73,28 @@ export default {
     setUseExperimental(state: boolean) {
         localStorage.setItem('useExperimental', JSON.stringify(state));
     },
+
+    getSettings(): Settings {
+        const storage = localStorage.getItem('settings');
+        if (storage === null) {
+            // Create settings
+            const defaulSettings = generateDefaultSettings();
+            localStorage.setItem('settings', JSON.stringify(defaulSettings));
+
+            return defaulSettings;
+        };
+
+        // Merge default with stored in case new settings get added
+        return {
+            ...generateDefaultSettings(),
+            ...JSON.parse(storage),
+        };
+    },
+
+    setSetting(setting: Partial<Settings>) {
+        localStorage.setItem('settings', JSON.stringify({
+            ...generateDefaultSettings(),
+            ...setting,
+        }));
+    }
 }
