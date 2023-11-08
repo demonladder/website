980fb57b3e45ed7e97b58537a494ecd7e41b30dc
DiversenSato <diversensato@gmail.com>
2023-11-08 17:13:48 +0100

    Fixed setting width

--- gddl/src/pages/root/settings/Settings.tsx
+++ gddl/src/pages/root/settings/Settings.tsx
@@ -5,9 +5,9 @@ import { NavButton } from '../../../components/ui/NavButton';
 export default function Settings() {
     return (
         <Container>
-            <div className='flex'>
+            <div className='flex max-xl:flex-col max-xl:gap-8'>
                 <div>{/*Extra div is required so the nested div doesn't shrink*/}
-                    <div className='flex flex-col w-80'>
+                    <div className='flex flex-col xl:w-80'>
                         <NavButton to='/settings/site'>Site settings</NavButton>
                         <NavButton to='/settings/profile'>Profile settings</NavButton>
                         <NavButton to='/settings/submission'>Submission settings</NavButton>
