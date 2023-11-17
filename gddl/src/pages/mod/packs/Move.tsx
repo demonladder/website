d28d277e4c8473bcbd86eee188ddce0b108a7f96
DiversenSato <diversensato@gmail.com>
2023-11-17 02:54:47 +0100

    Removed unused routes

--- gddl/src/pages/mod/packs/Move.tsx
+++
@@ -1,9 +0,0 @@
-import WarningBox from "../../../components/message/WarningBox";
-
-export default function Move() {
-    return (
-        <div>
-            <WarningBox text={'Doesn\'t work yet'} />
-        </div>
-    );
-}
