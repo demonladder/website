9507d982bd65779718340ada734bce72b9c22da2
DiversenSato <diversensato@gmail.com>
2024-08-26 01:02:10 +0200

    Added enjoyment spread

--- src/pages/root/level/RatingGraph.tsx
+++ src/pages/root/level/RatingGraph.tsx
@@ -15,6 +15,7 @@ export default function RatingGraph({ levelID }: Props) {
 
     const ratingData = data.rating;
     const maxCount = ratingData.reduce((acc, cur) => acc + cur.Count, 0);
+    const maxEnjoymentCount = 1.0* data.enjoyment.reduce((acc, cur) => acc + cur.Count, 0);
 
     return (
         <section className='mt-6'>
@@ -22,8 +23,8 @@ export default function RatingGraph({ levelID }: Props) {
             <table>
                 <tbody>
                     <tr>
-                        <th>Tier</th>
-                        <th className='w-full'>Percentage</th>
+                        <th></th>
+                        <th className='w-full'></th>
                     </tr>
                     {ratingData.map((d) => (
                         <tr key={d.Rating}>
@@ -33,6 +34,26 @@ export default function RatingGraph({ levelID }: Props) {
                             <td className='flex items-center'>
                                 <span className={'inline-block h-6 tier-' + d.Rating} style={{ width: `${d.Count / maxCount * 100}%` }} />
                                 <span className='ms-4 text-white'>{d.Count} ({(d.Count / maxCount * 100).toFixed(2)}%)</span>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <h2 className='text-3xl mt-6 mb-1'>Enjoyment spread</h2>
+            <table className='mt-2'>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th className='w-full'></th>
+                    </tr>
+                    {data.enjoyment.map((d) => (
+                        <tr key={d.Enjoyment}>
+                            <td>
+                                <p className='pe-2 whitespace-nowrap text-center'>{`${d.Enjoyment}`}/10</p>
+                            </td>
+                            <td className='flex items-center'>
+                                <span className={'inline-block h-6 enj-' + d.Enjoyment} style={{ width: `${d.Count / maxEnjoymentCount * 100}%` }} />
+                                <span className='ms-4 text-white'>{d.Count} ({(d.Count / maxEnjoymentCount * 100).toFixed(2)}%)</span>
                             </td>
                         </tr>
                     ))}
