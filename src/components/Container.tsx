6f8ef5bb2965e8fb47df5cbc5a88ac66b885bc9b
DiversenSato <diversensato@gmail.com>
2024-08-26 01:01:01 +0200

    Added opacity to Containers

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className={'container mx-auto my-6 px-14 py-8 bg-gray-800 round:rounded-3xl' + (className ? ' '+className : '')}>
+        <div className={'container bg-opacity-90 mx-auto my-6 px-14 py-8 bg-gray-800 round:rounded-3xl' + (className ? ' '+className : '')}>
             {children}
         </div>
     );
