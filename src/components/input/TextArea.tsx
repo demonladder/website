3fc47aba371ec3a2164bad404f869e5e1da5e658
DiversenSato <diversensato@gmail.com>
2024-09-04 01:06:59 +0200

    Invalid text areas

--- src/components/input/TextArea.tsx
+++ src/components/input/TextArea.tsx
@@ -2,6 +2,6 @@ import { forwardRef, DetailedHTMLProps, InputHTMLAttributes } from 'react';
 
 type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
 
-export default forwardRef<HTMLTextAreaElement, Props>((props, ref) => (
-    <textarea {...props} ref={ref} className='block bg-black bg-opacity-20 p-1 outline-none border-b-2 rounded-none resize-none h-32 w-full' />
+export default forwardRef<HTMLTextAreaElement, Props & { invalid?: boolean }>(({ invalid, ...props }, ref) => (
+    <textarea {...props} ref={ref} className={'block bg-black bg-opacity-20 p-1 outline-none border-b-2 rounded-none resize-none h-32 w-full' + (invalid ? ' border-red-600' : '')} />
 ));
