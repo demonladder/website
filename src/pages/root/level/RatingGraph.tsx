5a5569f79c9a9838c8028d7c22fd246b65eba0b8
DiversenSato <diversensato@gmail.com>
2024-10-14 16:37:54 +0200

    Fixed enjoyment spread not showing on platformers

--- src/pages/root/level/RatingGraph.tsx
+++ src/pages/root/level/RatingGraph.tsx
@@ -11,34 +11,38 @@ export default function RatingGraph({ levelID }: Props) {
         queryFn: () => GetSpread(levelID),
     });
 
-    if (data === undefined || data.rating.length <= 1) return;
+    if (data === undefined) return;
 
     const ratingData = data.rating;
     const maxCount = ratingData.reduce((acc, cur) => acc + cur.Count, 0);
-    const maxEnjoymentCount = 1.0* data.enjoyment.reduce((acc, cur) => acc + cur.Count, 0);
+    const maxEnjoymentCount = 1.0 * data.enjoyment.reduce((acc, cur) => acc + cur.Count, 0);
 
     return (
         <section className='mt-6'>
-            <h2 className='text-3xl mb-1'>Rating spread</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <th></th>
-                        <th className='w-full'></th>
-                    </tr>
-                    {ratingData.map((d) => (
-                        <tr key={d.Rating}>
-                            <td>
-                                <p className='pe-2 whitespace-nowrap'>{`Tier ${d.Rating}`}</p>
-                            </td>
-                            <td className='flex items-center'>
-                                <span className={'inline-block h-6 tier-' + d.Rating} style={{ width: `${d.Count / maxCount * 100}%` }} />
-                                <span className='ms-4 text-white'>{d.Count} ({(d.Count / maxCount * 100).toFixed(2)}%)</span>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {data.rating.length > 0 &&
+                <>
+                    <h2 className='text-3xl mb-1'>Rating spread</h2>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th></th>
+                                <th className='w-full'></th>
+                            </tr>
+                            {ratingData.map((d) => (
+                                <tr key={d.Rating}>
+                                    <td>
+                                        <p className='pe-2 whitespace-nowrap'>{`Tier ${d.Rating}`}</p>
+                                    </td>
+                                    <td className='flex items-center'>
+                                        <span className={`inline-block h-6 tier-${d.Rating}`} style={{ width: `${d.Count / maxCount * 100}%` }} />
+                                        <span className='ms-4 text-white'>{d.Count} ({(d.Count / maxCount * 100).toFixed(2)}%)</span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </>
+            }
             <h2 className='text-3xl mt-6 mb-1'>Enjoyment spread</h2>
             <table className='mt-2'>
                 <tbody>
@@ -52,7 +56,7 @@ export default function RatingGraph({ levelID }: Props) {
                                 <p className='pe-2 whitespace-nowrap text-center'>{`${d.Enjoyment}`}/10</p>
                             </td>
                             <td className='flex items-center'>
-                                <span className={'inline-block h-6 enj-' + d.Enjoyment} style={{ width: `${d.Count / maxEnjoymentCount * 100}%` }} />
+                                <span className={`inline-block h-6 enj-${d.Enjoyment}`} style={{ width: `${d.Count / maxEnjoymentCount * 100}%` }} />
                                 <span className='ms-4 text-white'>{d.Count} ({(d.Count / maxEnjoymentCount * 100).toFixed(2)}%)</span>
                             </td>
                         </tr>
