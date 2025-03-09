ee009df6d87caf69c4012c6f09e1f59e9d0c6107
DiversenSato <diversensato@gmail.com>
2025-03-09 21:33:15 +0100

    Updated package-lock.json

--- src/hooks/api/usePack.ts
+++ src/hooks/api/usePack.ts
@@ -1,14 +1,15 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query';
 import GetSinglePack from '../../api/pack/requests/GetSinglePack';
+import { AxiosError } from 'axios';
 
-interface Options {
-    enabled: UseQueryOptions['enabled'];
-}
-
-export default function usePack(packID: number, options?: Options) {
-    return useQuery({
-        queryKey: ['packs', packID],
-        queryFn: () => GetSinglePack(packID),
-        ...(options ?? {}),
-    });
+export default function usePack(packID: number, options?: UseQueryOptions<
+    Awaited<ReturnType<typeof GetSinglePack>>,
+    AxiosError,
+    Awaited<ReturnType<typeof GetSinglePack>>,
+    QueryKey
+>) {
+    options = options ?? {};
+    options.queryKey = ['packs', packID];
+    options.queryFn = () => GetSinglePack(packID);
+    return useQuery(options);
 }
