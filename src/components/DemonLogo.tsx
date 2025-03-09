a7693e3f3dfb695651f220a747f0760a0d926ac7
DiversenSato <diversensato@gmail.com>
2025-03-09 21:20:36 +0100

    Refactor DemonLogo component

--- src/components/DemonLogo.tsx
+++ src/components/DemonLogo.tsx
@@ -1,26 +1,8 @@
-export default function DemonLogo({ diff }: { diff?: number | string }) {
-    switch (diff) {
-        default:
-        case 'Easy':
-        case 1:
-            return (<img src='/assets/images/demon_logos/edemon.png' />);
-        case 'Medium':
-        case 2:
-            return (<img src='/assets/images/demon_logos/mdemon.png' />);
-        case 'Official':
-        case 'Hard':
-        case 3:
-            return (<img src='/assets/images/demon_logos/hdemon.png' />);
-        case 'Insane':
-        case 4:
-            return (<img src='/assets/images/demon_logos/idemon.png' />);
-        case 'Extreme':
-        case 5:
-            return (<img src='/assets/images/demon_logos/exdemon.png' />);
-    }
+export default function DemonLogo({ diff, ...props }: { diff?: number | string } & React.ImgHTMLAttributes<HTMLImageElement>) {
+    return (<img src={DifficultyToImgSrc(diff)} {...props} />);
 }
 
-export function DifficultyToImgSrc(diff: string|number) {
+export function DifficultyToImgSrc(diff?: string | number) {
     switch (diff) {
         case 'Easy':
         case 1:
