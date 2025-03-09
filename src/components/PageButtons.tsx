851b5669af93566a6e00dca70c59f05401be6c0b
DiversenSato <diversensato@gmail.com>
2025-03-09 21:23:54 +0100

    Refactor PageButtons component to simplify state management and improve page navigation logic

--- src/components/PageButtons.tsx
+++ src/components/PageButtons.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { clamp } from '../utils/clamp';
 import { PrimaryButton } from './Button';
 import { NumberInput } from './Input';
@@ -14,13 +14,8 @@ export type PageMeta = {
     limit: number,
 }
 
-// meta.page starts at 0
 export default function PageButtons({ onPageChange, meta }: Props) {
-    const [displayedValue, setDisplayedValue] = useState('' + (meta?.page || 0));
-
-    useEffect(() => {
-        setDisplayedValue('' + (meta?.page || 0));
-    }, [meta?.page]);
+    const [page, setPage] = useState(((meta?.page ?? 0) + 1).toString());
 
     if (meta === undefined) return;
 
@@ -29,32 +24,37 @@ export default function PageButtons({ onPageChange, meta }: Props) {
     const maxPages = Math.ceil(meta.total / meta.limit);
 
     function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.value === '') return setDisplayedValue('');
+        setPage(e.target.value);
+        if (e.target.value === '') return;
 
         const parsed = parseInt(e.target.value);
         if (isNaN(parsed)) return;
 
-        setDisplayedValue('' + clamp(parsed, 1, maxPages));
-        onPageChange(clamp(parsed, 1, maxPages));
+        onPageChange(clamp(parsed - 1, 1, maxPages));
+    }
+
+    function onPageButtonClick(page: number) {
+        onPageChange(page);
+        setPage((page + 1).toString());
     }
 
     return (
         <div className='mt-3 flex gap-3 justify-center items-center'>
-            {meta.page > 1 &&  // If the current page is not the first, render button
+            {meta.page > 0 &&  // If the current page is not the first, render button
                 <div className='flex gap-2'>
-                    <PrimaryButton onClick={() => onPageChange(1)}>{'<<'}</PrimaryButton>
-                    <PrimaryButton onClick={() => onPageChange(meta.page-1)}>{'<'}</PrimaryButton>
+                    <PrimaryButton onClick={() => onPageButtonClick(0)}>{'<<'}</PrimaryButton>
+                    <PrimaryButton onClick={() => onPageButtonClick(meta.page - 1)}>{'<'}</PrimaryButton>
                 </div>
             }
             <div className='flex items-center gap-1'>
                 <p className='m-0'>Page</p>
-                <NumberInput disableSpinner={true} centered={true} value={displayedValue} onChange={onChange} style={{width: '3rem'}} />
+                <NumberInput disableSpinner={true} centered={true} value={page} onChange={onChange} style={{ width: '3rem' }} />
                 <p>/{maxPages}</p>
             </div>
-            {meta.page < maxPages &&  // If the current page is not the last, render button
+            {(meta.page + 1) < maxPages &&  // If the current page is not the last, render button
                 <div className='flex gap-2'>
-                    <PrimaryButton onClick={() => onPageChange(meta.page+1)}>{'>'}</PrimaryButton>
-                    <PrimaryButton onClick={() => onPageChange(maxPages)}>{'>>'}</PrimaryButton>
+                    <PrimaryButton onClick={() => onPageButtonClick(meta.page + 1)}>{'>'}</PrimaryButton>
+                    <PrimaryButton onClick={() => onPageButtonClick(maxPages - 1)}>{'>>'}</PrimaryButton>
                 </div>
             }
         </div>
