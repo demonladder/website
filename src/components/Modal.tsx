7a9113c75b6550583149968b68c1154bea3e3654
DiversenSato <diversensato@gmail.com>
2025-05-25 13:02:39 +0200

    fix: update text color in Modal component for better visibility

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,7 +19,7 @@ export default function Modal({ title, show, children, onClose }: Props) {
 
     return (
         <div ref={backgroundRef} onClick={handleClose} className={'fixed z-30 inset-0 w-full h-full transition-colors ' + (show ? 'bg-black/40 ' : 'pointer-events-none')}>
-            <div style={{ maxHeight: 'calc(100% - 10rem)' }} className={'max-w-lg min-w-[314px] overflow-y-auto top-5 absolute left-1/2 -translate-x-1/2 round:rounded-xl bg-theme-600 px-6 py-4 opacity-0 transition-opacity text-base' + (show ? ' opacity-100 pointer-events-auto' : '')}>
+            <div style={{ maxHeight: 'calc(100% - 10rem)' }} className={'max-w-lg min-w-[314px] overflow-y-auto top-5 absolute left-1/2 -translate-x-1/2 round:rounded-xl bg-theme-600 text-theme-text px-6 py-4 opacity-0 transition-opacity text-base' + (show ? ' opacity-100 pointer-events-auto' : '')}>
                 <div>
                     <Heading1>{title}</Heading1>
                 </div>
