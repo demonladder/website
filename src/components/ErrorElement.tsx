5071ec9acaf8616b873097492f23f2ce7b9ec927
DiversenSato <diversensato@gmail.com>
2025-05-25 12:35:09 +0200

    fix: remove unused error handling in ErrorElement component

--- src/components/ErrorElement.tsx
+++ src/components/ErrorElement.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useRouteError } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { SecondaryButton } from './ui/buttons/SecondaryButton';
 import { Helmet } from 'react-helmet-async';
 import Header from '../layouts/header/Header';
@@ -8,7 +8,6 @@ import Heading1 from './headings/Heading1';
 
 export default function ErrorElement() {
     const navigate = useNavigate();
-    const error = useRouteError();
 
     return (
         <div className='relative flex flex-col'>
@@ -21,9 +20,6 @@ export default function ErrorElement() {
                     <div className='text-center'>
                         <Heading1>Shoot dang, something went wrong!</Heading1>
                         <p className='text-lg'>Is this where people typically say "404 not found"?</p>
-                        {error instanceof Error &&
-                            <pre>{error.message}</pre>
-                        }
                         <SecondaryButton onClick={() => navigate('/')}>Home</SecondaryButton>
                     </div>
                 </div>
