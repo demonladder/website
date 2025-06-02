a6086783a8de9b2298ee33627ef093d94c63fbeb
DiversenSato <diversensato@gmail.com>
2025-06-03 00:17:27 +0200

    feat: improve name filtering in edit submisisons admin page
    Adds name to query params and removes the debounced value

--- src/pages/mod/editSubmissions/EditSubmissions.tsx
+++ src/pages/mod/editSubmissions/EditSubmissions.tsx
@@ -6,9 +6,8 @@ import LoadingSpinner from '../../../components/LoadingSpinner';
 import useLevelSearch from '../../../hooks/useLevelSearch';
 import Submission from '../../../api/types/Submission';
 import { getLevelSubmissions } from '../../../features/level/api/getLevelSubmissions';
-import useLateValue from '../../../hooks/useLateValue';
 import FormInputLabel from '../../../components/form/FormInputLabel';
-import { NumberParam, useQueryParam, withDefault } from 'use-query-params';
+import { NumberParam, StringParam, useQueryParam, withDefault } from 'use-query-params';
 import EditableSubmission from './EditableSubmission';
 import { useSubmission } from '../../../components/modals/useSubmission';
 
@@ -17,13 +16,13 @@ export default function EditSubmission() {
     const [userID, setUserID] = useQueryParam('userID', NumberParam);
 
     const { activeLevel, SearchBox } = useLevelSearch({ ID: 'addSubmissionSearch', options: { defaultLevel: levelID } });
-    const [usernameFilter, lateUsernameFilter, setUsernameFilter] = useLateValue('');
+    const [username, setUsername] = useQueryParam('username', withDefault(StringParam, ''));
 
     const [page, setPage] = useQueryParam('page', withDefault(NumberParam, 0));
 
     const { status, data } = useQuery({
-        queryKey: ['level', activeLevel?.ID, 'submissions', { page, username: lateUsernameFilter, progressFilterKey: 'all' }],
-        queryFn: () => getLevelSubmissions({ levelID: activeLevel?.ID || 0, limit: 24, page, username: lateUsernameFilter, progressFilter: 'all' }),
+        queryKey: ['level', activeLevel?.ID, 'submissions', { page, username, progressFilterKey: 'all' }],
+        queryFn: () => getLevelSubmissions({ levelID: activeLevel?.ID || 0, limit: 24, page, username, progressFilter: 'all' }),
     });
 
     const { data: submission } = useSubmission(levelID ?? 0, userID ?? 0, {
@@ -34,7 +33,7 @@ export default function EditSubmission() {
         setPage(0);
         setLevelID(activeLevel?.ID);
         setUserID(undefined);
-    }, [activeLevel, setLevelID, setUserID]);
+    }, [activeLevel, setLevelID, setPage, setUserID]);
 
     function submissionClicked(s: Submission) {
         setUserID(s.UserID);
@@ -53,7 +52,7 @@ export default function EditSubmission() {
                 </div>
                 <div>
                     <p className='font-bold'>Submission list</p>
-                    <TextInput value={usernameFilter} onChange={(e) => setUsernameFilter(e.target.value)} placeholder='Filter by user name...' />
+                    <TextInput value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Filter by user name...' />
                     <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-2'>
                         {data?.submissions.map((s) => (
                             <button className={'flex ps-1 border border-white/0 hover:border-white/80 transition-colors select-none round:rounded ' + (s.UserID === userID ? 'bg-button-primary-1 font-bold' : 'bg-theme-600')} onClick={() => submissionClicked(s)} key={`edit_${s.UserID}_${s.LevelID}`}>
