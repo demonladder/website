9db9d7ace7e0af7ce6ec9d6d8cecd20d659e6969
DiversenSato <diversensato@gmail.com>
2025-06-02 02:39:24 +0200

    feat: enhance FilledButton component with size variants and improved styles

--- src/components/input/buttons/filled/FilledButton.tsx
+++ src/components/input/buttons/filled/FilledButton.tsx
@@ -2,10 +2,11 @@ import './filledButtonStyles.css';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children?: string | number;
+    sizeVariant?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
-export default function FilledButton({ children, ...props }: Props) {
+export default function FilledButton({ children, sizeVariant = 'sm', ...props }: Props) {
     return (
-        <button {...props} className={'filled-button ' + (props.className ?? '')}>{children}</button>
+        <button {...props} className={`filled-button ${sizeVariant} ` + (props.className ?? '')}>{children}</button>
     );
 }
