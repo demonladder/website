90ec46f656a73e09a655dbc0d720521332437b05
DiversenSato <diversensato@gmail.com>
2025-06-19 22:30:28 +0200

    fix: refactor Search component to use forwardRef for input reference

--- src/components/input/search/Search.tsx
+++ src/components/input/search/Search.tsx
@@ -1,14 +1,16 @@
+import { forwardRef } from 'react';
 import './search.css';
 
 interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
     onMenu?: (e: React.MouseEvent) => void;
 }
 
-export default function Search({ onMenu, placeholder, ...props }: Props) {
+const Search = forwardRef<HTMLInputElement, Props>(function Search({ onMenu, placeholder, ...props }: Props, ref) {
     return (
         <div className='search'>
             <i className='bx bx-filter text-2xl cursor-pointer' onClick={onMenu} />
-            <input type='text' {...props} placeholder={placeholder ?? 'Search...'} />
+            <input type='text' {...props} ref={ref} placeholder={placeholder ?? 'Search...'} />
         </div>
     );
-}
+});
+export default Search;
