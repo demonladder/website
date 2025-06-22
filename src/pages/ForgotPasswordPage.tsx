a910ac40cd9ade900f41798139c2de697c6cd192
DiversenSato <diversensato@gmail.com>
2025-06-22 02:51:34 +0200

    refactor: enhance ForgotPasswordPage UI by updating text and adding input description

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -9,6 +9,7 @@ import { Id, toast } from 'react-toastify';
 import renderToastError from '../utils/renderToastError';
 import { AxiosError } from 'axios';
 import { forgotPassword } from '../api/auth/forgotPassword';
+import FormInputDescription from '../components/form/FormInputDescription';
 
 export default function ForgotPassword() {
     const [username, setUsername] = useState('');
@@ -32,13 +33,17 @@ export default function ForgotPassword() {
             <div className='flex justify-center'>
                 <div className='w-11/12 md:w-1/2 lg:w-2/6'>
                     <Heading1 className='mb-4'>Forgot password?</Heading1>
-                    <p>Enter your Discord username below and you will be sent a link to reset your password.</p>
-                    <p>If you have not linked your GDDL account to Discord, join <a href='https://discord.gg/gddl'>our Discord</a> and create a thread in <b>#support</b>.</p>
+                    <p>Enter your <b>Discord username</b> below and you will be sent a link to reset your password.</p>
+                    <p>You must share a server with our bot and have DMs open!</p>
+                    <p className='my-2'>If you have not linked your GDDL account to Discord, join <a className='underline text-blue-400' href='https://discord.gg/gddl'>our Discord</a>, create a thread in <b>#support</b> and we'll get you sorted out.</p>
                     <form onSubmit={onSubmit}>
                         <FormGroup>
-                            <TextInput value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Discord username' autoComplete='discord username' invalid={username.match(/^[a-zA-Z0-9._]{2,32}$/) === null} />
+                            <TextInput value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username...' autoComplete='new-password' invalid={username.match(/^[a-zA-Z0-9._]{2,32}$/) === null} />
+                            <FormInputDescription>Case sensitive</FormInputDescription>
                         </FormGroup>
-                        <PrimaryButton type='submit' disabled={mutation.isPending}>Send</PrimaryButton>
+                        <FormGroup>
+                            <PrimaryButton type='submit' disabled={mutation.isPending}>Send</PrimaryButton>
+                        </FormGroup>
                     </form>
                 </div>
             </div>
