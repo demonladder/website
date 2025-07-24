f317d9bb5efaf89dcaf850a37d91cece668abe47
DiversenSato <diversensato@gmail.com>
2025-07-24 03:52:45 +0200

    fix: improve event handling and display in AuditLog component (WIP)

--- src/features/admin/audit-logs/components/AuditLog.tsx
+++ src/features/admin/audit-logs/components/AuditLog.tsx
@@ -10,27 +10,38 @@ interface Props {
 function getEvent(log: IAuditLog) {
     switch (log.event) {
         case AuditEvents.PACK_CREATE:
-            return <><b>{log.userID}</b> created pack <b>{log.changes?.find((change) => change.key === 'Name')?.newValue}</b></>
+            return <><b>{log.userID}</b> created pack <b>{log.changes?.find((change) => change.key === 'Name')?.newValue}</b></>;
         case AuditEvents.PACK_DELETE:
-            return <><b>{log.userID}</b> deleted pack <b>{log.changes?.find((change) => change.key === 'Name')?.oldValue}</b></>
+            return <><b>{log.userID}</b> deleted pack <b>{log.changes?.find((change) => change.key === 'Name')?.oldValue}</b></>;
         case AuditEvents.USER_UPDATE:
-            return <><b>{log.userID}</b> updated user <b>{log.targetID}</b></>
+            return <><b>{log.userID}</b> updated user <b>{log.targetID}</b></>;
+        case AuditEvents.PASSWORD_RESET_LINK_CREATE:
+            return <><b>{log.userID}</b> created a password reset link for themselves</>;
         default:
-            return 'Unknown event';
+            return `Unknown event (${log.event})`;
     }
 }
 
 export default function AuditLog({ log }: Props) {
     const [showDetails, setShowDetails] = useState(false);
 
+    const hasChanges = (log.changes?.length ?? 0) > 0;
+
     return (
-        <div className='border border-theme-500 bg-theme-800 p-2 my-2 round:rounded-xl cursor-pointer' onClick={() => setShowDetails((prev) => (log.changes?.length ?? 0) > 0 && !prev)}>
-            <div className='flex gap-2'>
-                <img className='rounded-full size-14' src={`${import.meta.env.VITE_SERVER_URL}/api/user/${log.userID}/discord-pfp`} alt='' />
-                <div>
-                    <Heading3>{getEvent(log)}</Heading3>
-                    <p className='text-theme-400'>{new Date(log.createdAt.replace(' +00:00', 'Z').replace(' ', 'T')).toLocaleString()}</p>
+        <div className={'border border-theme-500 bg-theme-800 p-2 my-2 round:rounded-xl ' + (hasChanges ? 'cursor-pointer' : '')} onClick={() => setShowDetails((prev) => hasChanges && !prev)}>
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-2'>
+                    <img className='rounded-full size-14' src={`${import.meta.env.VITE_SERVER_URL}/api/user/${log.userID}/discord-pfp`} alt='' />
+                    <div>
+                        <Heading3>{getEvent(log)}</Heading3>
+                        <p className='text-theme-400'>{new Date(log.createdAt.replace(' +00:00', 'Z').replace(' ', 'T')).toLocaleString()}</p>
+                    </div>
                 </div>
+                {hasChanges && (
+                    showDetails
+                        ? <i className='bx bx-chevron-up ms-auto self-center text-2xl' />
+                        : <i className='bx bx-chevron-down ms-auto self-center text-2xl' />
+                )}
             </div>
             {showDetails &&
                 <ul>
