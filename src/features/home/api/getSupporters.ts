1c0b149fafa7cf059b0cb4b7d279e2ce7405a268
DiversenSato <diversensato@gmail.com>
2025-08-14 03:27:54 +0200

    revert: remove rank from supporters

--- src/features/home/api/getSupporters.ts
+++ src/features/home/api/getSupporters.ts
@@ -1,6 +1,6 @@
 import APIClient from '../../../api/APIClient';
 
 export async function getSupporters() {
-    const res = await APIClient.get<{ fromName: string, rank: number, displayAmount: string }[]>('/kofi/supporters');
+    const res = await APIClient.get<{ ID: number, name: string }[]>('/kofi/supporters');
     return res.data;
 }
--- src/features/home/components/Supporters.tsx
+++ src/features/home/components/Supporters.tsx
@@ -2,23 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import Heading2 from '../../../components/headings/Heading2';
 import Surface from '../../../components/Surface';
 import { getSupporters } from '../api/getSupporters';
+import { Link } from 'react-router-dom';
 
-function Supporter({ supporter, rank }: { supporter: { fromName: string, displayAmount: string }, rank: number }) {
-    let classNames = 'mt-2 me-2';
-
-    classNames += {
-        1: ' text-amber-400 text-3xl',
-        2: ' text-neutral-400 text-2xl',
-        3: ' text-orange-400 text-xl mb-4',
-        4: '',
-    }[Math.min(rank, 4)];
-
+function Supporter({ supporter }: { supporter: { ID: number, name: string } }) {
     return (
-        <li className={classNames}>
-            <div className='flex justify-between'>
-                <p>#{rank} <b>{supporter.fromName}</b></p>
-                <p>{supporter.displayAmount}</p>
-            </div>
+        <li>
+            <p className='text-xl px-2 py-1 text-yellow-400 bg-theme-700 rounded'><b><Link to={`/profile/${supporter.ID}`}>{supporter.name}</Link></b></p>
         </li>
     );
 }
@@ -30,12 +19,12 @@ export default function Supporters() {
     });
 
     return (
-        <Surface variant='800'>
+        <Surface variant='800' size='xl'>
             <Heading2 className='mb-4 text-center'>Supporters</Heading2>
             {status === 'success' &&
-                <ul className='overflow-y-auto max-h-[400px]' style={{ scrollbarWidth: 'thin' }}>
-                    {supporters.map((supporter, i) => (
-                        <Supporter supporter={supporter} rank={i + 1} key={supporter.fromName} />
+                <ul className='flex gap-2 overflow-y-auto max-h-[400px]' style={{ scrollbarWidth: 'thin' }}>
+                    {supporters.map((supporter) => (
+                        <Supporter supporter={supporter} key={supporter.ID} />
                     ))}
                 </ul>
             }
