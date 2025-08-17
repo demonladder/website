1bf750e5b39926fcaf449ae2f5de9f93fa1ae3b7
DiversenSato <diversensato@gmail.com>
2025-08-17 21:19:56 +0200

    feat: make deny and delete reasons required

--- src/api/submissions/DeletePendingSubmission.ts
+++ src/api/submissions/DeletePendingSubmission.ts
@@ -1,5 +1,5 @@
 import APIClient from '../APIClient';
 
-export default async function DeletePendingSubmission(ID: number) {
-    await APIClient.delete(`/submissions/pending/${ID}`);
+export default async function DeletePendingSubmission(ID: number, reason?: string) {
+    await APIClient.delete(`/submissions/pending/${ID}`, { data: { reason } });
 }
