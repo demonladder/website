f66441a9cdf0402e39551f63afa17993b0672416
DiversenSato <diversensato@gmail.com>
2025-08-21 12:47:56 +0200

    style: make profile dropdown styled like context menus

--- src/components/ProfileButtons.tsx
+++ src/components/ProfileButtons.tsx
@@ -48,12 +48,14 @@ function ProfileButton({ userID, size = 'large' }: Props) {
                     </div>
                 </Link>
                 <div className='max-2xl:hidden absolute opacity-0 right-0 z-40 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity'>
-                    <ul className='mt-1 bg-theme-600 text-theme-text shadow round:rounded-lg flex flex-col whitespace-nowrap'>
-                        <li><Link className='p-2 block round:rounded-t-lg hover:bg-theme-950/40' to={`/profile/${userID}`}><i className='bx bxs-user me-1' /> Go to profile</Link></li>
-                        <li><Link className='p-2 block hover:bg-theme-950/40' to='/notifications'> <i className='bx bx-bell me-1' />Notifications</Link></li>
-                        <li><Link className='p-2 block hover:bg-theme-950/40' to='/settings/profile'> <i className='bx bx-cog me-1' />Settings</Link></li>
-                        {session.hasPermission(PermissionFlags.STAFF_DASHBOARD) && <li><Link className='p-2 block hover:bg-theme-950/40' to='/mod'><i className='bx bxs-dashboard me-1' /> Staff dashboard</Link></li>}
-                        <li><p className='p-2 round:rounded-b-lg hover:bg-theme-950/40 cursor-pointer transition-colors' onClick={() => void session.logout()}><i className='bx bx-log-out me-1' /> Log out</p></li>
+                    <ul className='mt-1 p-1 bg-theme-900 border border-theme-400 text-theme-text shadow round:rounded-lg flex flex-col gap-1 whitespace-nowrap'>
+                        <li><Link className='pe-4 py-1 block rounded-lg hover:bg-theme-700' to={`/profile/${userID}`}><i className='bx bxs-user mx-2' /> Profile</Link></li>
+                        <li><Link className='pe-4 py-1 rounded-lg block hover:bg-theme-700' to='/notifications'><i className='bx bx-bell mx-2' /> Notifications</Link></li>
+                        <li><Link className='pe-4 py-1 rounded-lg block hover:bg-theme-700' to='/settings/profile'><i className='bx bx-cog mx-2' /> Settings</Link></li>
+                        {session.hasPermission(PermissionFlags.STAFF_DASHBOARD) &&
+                            <li><Link className='pe-4 py-1 rounded-lg block hover:bg-theme-700' to='/mod'><i className='bx bxs-dashboard mx-2' /> Dashboard</Link></li>
+                        }
+                        <li><p className='pe-4 py-1 rounded-lg hover:bg-theme-700 cursor-pointer transition-colors' onClick={() => void session.logout()}><i className='bx bx-log-out mx-2' /> Log out</p></li>
                     </ul>
                 </div>
             </div>
