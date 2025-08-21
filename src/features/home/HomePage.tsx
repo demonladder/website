5c48a34048416e5e95a2aae86e2fe6d73f194c56
DiversenSato <diversensato@gmail.com>
2025-08-21 13:04:31 +0200

    fix: auto scale anniversary video by window size

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -8,13 +8,27 @@ import TrendingLevels from './components/TrendingLevels';
 import PopularLevels from './components/PopularLevels';
 import Supporters from './components/Supporters';
 import Heading2 from '../../components/headings/Heading2';
+import { useWindowSize } from 'usehooks-ts';
+import Heading1 from '../../components/headings/Heading1';
 
 export default function Home() {
     const navigate = useNavigate();
 
+    const winSize = useWindowSize();
+
+    const margin = ((width: number): number => {
+        if (width >= 1536) return width - 1536;
+        if (width >= 1280) return width - 1280;
+        if (width >= 1024) return width - 1024;
+        if (width >= 768) return width - 768;
+        if (width >= 560) return width - 560;
+        return 0;
+    })(winSize.width);
+
     return (
         <main>
-            <iframe className='mx-auto my-10' width='1344' height='756' src='https://www.youtube.com/embed/wbGyG7ovHaA' title='GDDL 5' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowFullScreen />
+            <div className='container mx-auto my-10'><Heading1>Celebrating GDDLs 5 year anniversary</Heading1></div>
+            <iframe className='mx-auto' width={winSize.width - margin} height={(winSize.width - margin) / 16 * 9} src='https://www.youtube.com/embed/wbGyG7ovHaA' title='GDDL 5' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowFullScreen />
             <div className='container mx-auto py-4 grid grid-cols-1 xl:grid-cols-4 gap-8'>
                 <Container className='xl:col-span-3'>
                     <Announcement>
