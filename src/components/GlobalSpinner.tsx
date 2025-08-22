0f5c119bfb8760b435892a0e0305638220b1de62
DiversenSato <diversensato@gmail.com>
2025-08-22 13:16:53 +0200

    fix: add spinner to global spinner

--- src/components/GlobalSpinner.tsx
+++ src/components/GlobalSpinner.tsx
@@ -1,5 +1,7 @@
 export default function GlobalSpinner() {
     return (
-        <div className='fixed inset-0 bg-black z-50' />
+        <div className='fixed inset-0 bg-black/35 grid place-items-center' style={{ zIndex: 100 }}>
+            <i className='bx bx-loader-alt bx-spin text-9xl' />
+        </div>
     );
 }
