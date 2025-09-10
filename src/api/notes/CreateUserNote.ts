2ba2666ac70a37ee054ee9dd137cbf68b4a7fcd8
DiversenSato <diversensato@gmail.com>
2025-09-10 18:18:36 +0200

    fix: correct note endpoints

--- src/api/notes/CreateUserNote.ts
+++ src/api/notes/CreateUserNote.ts
@@ -1,7 +1,7 @@
 import APIClient from '../APIClient';
 
 export default async function CreateUserNote(targetID: number, content: string) {
-    await APIClient.post(`/notes/`, {
+    await APIClient.post(`/notes`, {
         targetID,
         content,
     });
--- src/api/notes/GetUserNotes.ts
+++ src/api/notes/GetUserNotes.ts
@@ -2,6 +2,6 @@ import APIClient from '../APIClient';
 import Note from '../types/Note';
 
 export default async function GetUserNotes(targetID: number) {
-    const res = await APIClient.get<Note[]>(`/notes/target/${targetID}`);
+    const res = await APIClient.get<Note[]>('/notes', { params: { userID: targetID } });
     return res.data;
 }
