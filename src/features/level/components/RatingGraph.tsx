06bea2872ec9893a1dca50bfcb2b82b29768154e
DiversenSato <diversensato@gmail.com>
2025-09-25 19:53:34 +0200

    fix(Level): hide tier distribution if there are no tier ratings

--- src/features/level/components/RatingGraph.tsx
+++ src/features/level/components/RatingGraph.tsx
@@ -54,64 +54,70 @@ export default function RatingGraph({ levelMeta, twoPlayer, setShowTwoPlayerStat
         return filledData.sort((a, b) => a.Enjoyment - b.Enjoyment);
     }, [enjoymentData]);
 
+    if (ratingData?.length === 0 && enjoymentData?.length === 0) return;
+
     return (
         <section className='mt-6 grid grid-cols-1 lg:grid-cols-2 gap-6 border-gray-500 border-y py-6' ref={ref}>
             {status === 'pending' && <LoadingSpinner />}
             {status === 'success' &&
-                <div>
-                    <Heading2>Rating spread</Heading2>
-                    <TwoPlayerButtons levelMeta={levelMeta} showTwoPlayerStats={twoPlayer} setShowTwoPlayerStats={setShowTwoPlayerStats} />
-                    <table className='mt-2'>
-                        <tbody>
-                            <tr>
-                                <th></th>
-                                <th></th>
-                                <th className='w-full'></th>
-                            </tr>
-                            {filledRatingData.map((d) => (
-                                <tr key={d.Rating}>
-                                    <td className='border-e pe-3'>
-                                        <p className='pe-2 whitespace-nowrap text-center'>{d.Rating}</p>
-                                    </td>
-                                    <td className='ps-4 pe-2 text-right cursor-pointer select-none' onClick={() => setShowPercentages((prev) => !prev)}>
-                                        <p>{showPercentages ? `${(d.Count / totalRatings * 100).toFixed(1)}%` : d.Count}</p>
-                                    </td>
-                                    <td className='flex items-center'>
-                                        <span className={`inline-block h-6 tier-${d.Rating} rounded shadow`} style={{ width: `${d.Count / maxRatingCount * 100}%` }} />
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            }
-            {(enjoymentData ?? []).length > 0 &&
-                <div>
-                    <Heading2>Enjoyment spread</Heading2>
-                    <TwoPlayerButtons levelMeta={levelMeta} showTwoPlayerStats={twoPlayer} setShowTwoPlayerStats={setShowTwoPlayerStats} />
-                    <table className='mt-2'>
-                        <tbody>
-                            <tr>
-                                <th></th>
-                                <th></th>
-                                <th className='w-full'></th>
-                            </tr>
-                            {filledEnjoymentData.map((d) => (
-                                <tr key={d.Enjoyment}>
-                                    <td className='border-e pe-3'>
-                                        <p className='pe-2 whitespace-nowrap text-center'>{`${d.Enjoyment}`}</p>
-                                    </td>
-                                    <td className='ps-4 pe-2 text-right cursor-pointer select-none' onClick={() => setShowPercentages((prev) => !prev)}>
-                                        <p>{showPercentages ? `${(d.Count / totalEnjoyments * 100).toFixed(1)}%` : d.Count}</p>
-                                    </td>
-                                    <td className='flex items-center'>
-                                        <span className={`inline-block h-6 enj-${d.Enjoyment} rounded shadow`} style={{ width: `${d.Count / maxEnjoymentCount * 100}%` }} />
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                <>
+                    {(ratingData ?? []).length > 0 &&
+                        <div>
+                            <Heading2>Rating spread</Heading2>
+                            <TwoPlayerButtons levelMeta={levelMeta} showTwoPlayerStats={twoPlayer} setShowTwoPlayerStats={setShowTwoPlayerStats} />
+                            <table className='mt-2'>
+                                <tbody>
+                                    <tr>
+                                        <th></th>
+                                        <th></th>
+                                        <th className='w-full'></th>
+                                    </tr>
+                                    {filledRatingData.map((d) => (
+                                        <tr key={d.Rating}>
+                                            <td className='border-e pe-3'>
+                                                <p className='pe-2 whitespace-nowrap text-center'>{d.Rating}</p>
+                                            </td>
+                                            <td className='ps-4 pe-2 text-right cursor-pointer select-none' onClick={() => setShowPercentages((prev) => !prev)}>
+                                                <p>{showPercentages ? `${(d.Count / totalRatings * 100).toFixed(1)}%` : d.Count}</p>
+                                            </td>
+                                            <td className='flex items-center'>
+                                                <span className={`inline-block h-6 tier-${d.Rating} rounded shadow`} style={{ width: `${d.Count / maxRatingCount * 100}%` }} />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                    {(enjoymentData ?? []).length > 0 &&
+                        <div>
+                            <Heading2>Enjoyment spread</Heading2>
+                            <TwoPlayerButtons levelMeta={levelMeta} showTwoPlayerStats={twoPlayer} setShowTwoPlayerStats={setShowTwoPlayerStats} />
+                            <table className='mt-2'>
+                                <tbody>
+                                    <tr>
+                                        <th></th>
+                                        <th></th>
+                                        <th className='w-full'></th>
+                                    </tr>
+                                    {filledEnjoymentData.map((d) => (
+                                        <tr key={d.Enjoyment}>
+                                            <td className='border-e pe-3'>
+                                                <p className='pe-2 whitespace-nowrap text-center'>{`${d.Enjoyment}`}</p>
+                                            </td>
+                                            <td className='ps-4 pe-2 text-right cursor-pointer select-none' onClick={() => setShowPercentages((prev) => !prev)}>
+                                                <p>{showPercentages ? `${(d.Count / totalEnjoyments * 100).toFixed(1)}%` : d.Count}</p>
+                                            </td>
+                                            <td className='flex items-center'>
+                                                <span className={`inline-block h-6 enj-${d.Enjoyment} rounded shadow`} style={{ width: `${d.Count / maxEnjoymentCount * 100}%` }} />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                </>
             }
         </section>
     );
