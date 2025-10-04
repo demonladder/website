cc3a3f788007419a3009c73ea4bc299228a5ed61
DiversenSato <diversensato@gmail.com>
2025-10-04 18:00:44 +0200

    feat: make discord sync button redirect to api

--- src/features/settings/profile/components/DiscordSync.tsx
+++ src/features/settings/profile/components/DiscordSync.tsx
@@ -8,7 +8,7 @@ export default function DiscordSync() {
             <div className='mb-2'>
                 <DiscordProfilePicture />
             </div>
-            <DiscordLink href={import.meta.env.VITE_DISCORD_OAUTH}>Sync to Discord</DiscordLink>
+            <DiscordLink>Sync to Discord</DiscordLink>
             <p className='text-sm text-gray-400'>By syncing your account, GDDL will use your discord pfp instead of a demon face of your hardest.</p>
             <p className='text-sm text-gray-400'>GDDL only stores your Discord ID, username, pfp hash and accent color.</p>
             <p className='text-sm text-gray-400'>Should you change any of these, please re-sync in order to reflect the change here.</p>
