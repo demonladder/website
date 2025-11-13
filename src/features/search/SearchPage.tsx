70e04b683f735bac3d932c094b0e5b6d68f65f25
DiversenSato <diversensato@gmail.com>
2025-11-13 18:29:32 +0100

    fix: big x

--- src/features/search/SearchPage.tsx
+++ src/features/search/SearchPage.tsx
@@ -245,7 +245,7 @@ export default function Search() {
 function FilterLabel({ label, onRemove }: { label: string, onRemove: () => void }) {
     return (
         <button className='bg-theme-500 px-1 mx-1 rounded-md border border-theme-400 hover:border-red-500 group slow-effect-transition' onClick={onRemove}>
-            {label} <span className='mx-1 group-hover:text-red-500 slow-effect-transition font-bold'>x</span>
+            {label} <span className='mx-1 group-hover:text-red-500 slow-effect-transition font-bold'>X</span>
         </button>
     );
 }
