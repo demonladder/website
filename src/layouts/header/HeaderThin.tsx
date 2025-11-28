58ade928abf500b4ce6053b9b74ed282447cc105
DiversenSato <diversensato@gmail.com>
2025-11-29 00:18:04 +0100

    feat(HeaderThin): add 'Submit' link to navigation

--- src/layouts/header/HeaderThin.tsx
+++ src/layouts/header/HeaderThin.tsx
@@ -8,6 +8,7 @@ import useSession from '../../hooks/useSession';
 import { SecondaryButton } from '../../components/ui/buttons/SecondaryButton';
 import { PermissionFlags } from '../../features/admin/roles/PermissionFlags';
 import { useNotifications } from '../../features/notifications/hooks/useNotifications';
+import { routes } from '../../routes/route-definitions';
 
 export default function HeaderThin() {
     const [navOpen, setNavOpen] = useState(false);
@@ -35,7 +36,7 @@ export default function HeaderThin() {
             </div>
             <div className='basis-full grow px-16 grid overflow-hidden transition-[grid-template-rows]' style={{ gridTemplateRows: navOpen ? '1fr' : '0fr' }}>
                 <nav className='min-h-0 flex flex-col justify-between'>
-                    <div className='flex flex-col gap-x-3 gap-y-1 text-xl pt-4'>
+                    <div className='flex max-sm:flex-col gap-x-3 gap-y-1 text-xl pt-4'>
                         {HeaderRoutes.map((route, i) => (
                             <NavItem onClose={() => setNavOpen(false)} route={route} size={'thin'} key={`headerRoute_${i}`} />
                         ))}
@@ -47,6 +48,7 @@ export default function HeaderThin() {
                         {session.hasPermission(PermissionFlags.STAFF_DASHBOARD) && <Link className='text-xl' to='/mod' onClick={() => setNavOpen(false)}><i className='bx bx-shield-quarter' /> Dashboard</Link>}
                         <Link to='/notifications' className='text-xl' onClick={() => setNavOpen(false)}><i className={`bx ${notifications.data?.filter((n) => !n.IsRead).length ? 'bxs-bell' : 'bx-bell'}`} /> Notifications</Link>
                         <ProfileButtons onClick={() => setNavOpen(false)} size='small' />
+                        <Link to={routes.submit.href()} className='bg-blue-600 text-center text-white px-2 py-1 rounded-lg'>Submit</Link>
                         {session.user && <SecondaryButton onClick={() => void session.logout()}>Log out</SecondaryButton>}
                     </div>
                 </nav>
