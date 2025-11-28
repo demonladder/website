e15d297aa22907a583b0dccf13fca0575f6249f7
DiversenSato <diversensato@gmail.com>
2025-11-28 16:32:28 +0100

    fix(DiscordProfilePicture): update avatar image URL to include user ID

--- src/components/DiscordProfilePicture.tsx
+++ src/components/DiscordProfilePicture.tsx
@@ -8,7 +8,7 @@ export default function DiscordProfilePicture() {
     return (
         <div>
             {session.user?.avatar
-                ? <img src={`https://cdn.gdladder.com/avatars/${session.user.avatar}.png`} width='128' height='128' className='rounded-full' alt='Profile' />
+                ? <img src={`https://cdn.gdladder.com/avatars/${session.user.ID}/${session.user.avatar}.png`} width='128' height='128' className='rounded-full' alt='Profile picture' />
                 : <DemonFace diff={session.user?.Hardest?.Meta.Difficulty} size={DemonLogoSizes.LARGE} />
             }
         </div>
