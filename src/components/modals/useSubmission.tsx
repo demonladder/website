db18afc9ba1c834e5094fb358b8fcb2f5582d8a6
DiversenSato <diversensato@gmail.com>
2026-02-13 01:48:04 +0100

    fix(useSubmission): set placeholderData to undefined
    Fixes #3

--- src/components/modals/useSubmission.tsx
+++ src/components/modals/useSubmission.tsx
@@ -3,10 +3,17 @@ import GetSingleSubmission from '../../api/submissions/GetSingleSubmission';
 import { AxiosError } from 'axios';
 import { UseQueryOptionsWithoutKey } from '../../types/UseQueryOptionsWithoutKey';
 
-export function useSubmission(levelID: number, userID: number | undefined, options: UseQueryOptionsWithoutKey<Awaited<ReturnType<typeof GetSingleSubmission>>, AxiosError>) {
+export function useSubmission(
+    levelID: number,
+    userID: number | undefined,
+    options: UseQueryOptionsWithoutKey<Awaited<ReturnType<typeof GetSingleSubmission>>, AxiosError>,
+) {
     return useQuery({
         ...options,
         queryKey: ['submission', levelID, userID],
         queryFn: () => GetSingleSubmission(levelID, userID),
+        placeholderData: () => {
+            return undefined;
+        },
     });
 }
