f178cc120eaba5acec1df4c98529c2bd5e3b9430
DiversenSato <diversensato@gmail.com>
2026-02-14 04:13:51 +0100

    refactor: move stats api calls to client

--- src/api/stats/statsClient.ts
+++ src/api/stats/statsClient.ts
@@ -3,11 +3,44 @@ import type StatRecord from '../types/StatRecord';
 import type { Metrics } from './metrics';
 import { padDataSet } from './padDataSet';
 
+export interface GetStatsResponse {
+    users: {
+        now: number;
+        old: number;
+    };
+    registeredUsers: number;
+    activeUsers: number;
+    totalLevels: {
+        now: number;
+        old: number;
+    };
+    totalRatedLevels: number;
+    submissions: {
+        now: number;
+        old: number;
+    };
+    pendingSubmissions: {
+        now: number;
+        old: number;
+    };
+    recentSubmissions: number;
+    oldestQueuedSubmission?: string;
+    topRaters: {
+        UserID: number;
+        Name: string;
+    }[];
+}
+
 export interface GetQueueResponse {
     distributionByTier: { Tier: number; TierCount: number }[];
 }
 
 class StatsClient {
+    async getStats() {
+        const res = await APIClient.get<GetStatsResponse>('/stats');
+        return res.data;
+    }
+
     async getQueue() {
         const res = await APIClient.get<GetQueueResponse>('/stats/queue');
         return res.data;
