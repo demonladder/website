1c962c51d3d79abc1dcf503bf6ab5fcb52d5067a
DiversenSato <diversensato@gmail.com>
2026-03-20 00:07:16 +0100

    fix(DiscordLink): update href to correct Discord API endpoint

--- src/components/ui/buttons/DiscordLink.tsx
+++ src/components/ui/buttons/DiscordLink.tsx
@@ -4,7 +4,7 @@ export function DiscordLink(props: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAn
     return (
         <a
             {...props}
-            href='/api/oauth/2/discord'
+            href='/api/connections/discord'
             style={{ borderRadius: '3px', padding: '7px 16px' }}
             className={
                 'w-full text-center text-white bg-button-discord-primary hover:bg-button-discord-hover active:bg-button-discord-active transition-colors inline-block my-1' +
