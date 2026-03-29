2913b8ae108d7f9134a5285fc0ff40732df4b09f
DiversenSato <diversensato@gmail.com>
2026-03-30 01:37:54 +0200

    enhancement(useNotifications): reduce stale time to 15 minutes

--- src/features/notifications/hooks/useNotifications.ts
+++ src/features/notifications/hooks/useNotifications.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import GetNotifications from '../../../api/notifications/GetNotificationsRequest';
 import useSession from '../../../hooks/useSession';
+import ms from 'ms';
 
 export function useNotifications() {
     const session = useSession();
@@ -8,6 +9,7 @@ export function useNotifications() {
     return useQuery({
         queryKey: ['notifications'],
         queryFn: () => GetNotifications(),
+        staleTime: ms('15m'),
         enabled: session.user !== undefined,
     });
 }
